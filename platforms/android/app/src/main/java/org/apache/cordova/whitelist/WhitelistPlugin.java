aa1ee1d348a81d03db1310b059d8a13a90ee4582
12sivaeteja <sivateja.d@sreenidhi1sports.com>
2024-02-01 15:11:37 +0530

    added the changes

--- platforms/android/app/src/main/java/org/apache/cordova/whitelist/WhitelistPlugin.java
+++ platforms/android/app/src/main/java/org/apache/cordova/whitelist/WhitelistPlugin.java
@@ -22,7 +22,6 @@ package org.apache.cordova.whitelist;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.ConfigXmlParser;
 import org.apache.cordova.LOG;
-import org.apache.cordova.Whitelist;
 import org.xmlpull.v1.XmlPullParser;
 
 import android.content.Context;
